bdae75a3e951716ef859087f694354509bcc5811
vuledev <77647824+mystixxx@users.noreply.github.com>
2025-10-13 03:20:31 +0200

    images fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: {
+    unoptimized: true, // Needed for Static Export
+  },
 };
 
 export default nextConfig;
